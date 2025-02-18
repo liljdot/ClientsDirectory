3013cb28d493919442debeef1f195883c5e6ee39
liljdot <jehuoki@gmail.com>
2025-02-18 21:42:22 +0100

    hookup client table list with api, build out fetch ui

--- client/src/components/TableList.tsx
+++ client/src/components/TableList.tsx
@@ -1,14 +1,42 @@
 import useModalFormContext from "../hooks/useModalFormContext"
 import { Client } from "../types"
+import { useQuery } from '@tanstack/react-query'
 
 const TableList: React.FC = () => {
-    const clients: Client[] = [
-        { name: "John Doe", email: "john.doe@gmail.com", job: "Developer", isActive: true, rate: "100", id: "1" },
-        { name: "Jane Doe", email: "jane.doe@gmail.com", job: "Developer", isActive: true, rate: "101", id: "2" },
-        { name: "Naomi Jack", email: "naomi.jack@gmail.com", job: "Developer", isActive: false, rate: "102", id: "3" }
-    ]
+    let clients: Client[] = []
 
-    const {modalFormDispatch} = useModalFormContext()
+    const { modalFormDispatch } = useModalFormContext()
+    const { data, error, isLoading, refetch, isError } = useQuery<{ status: number, message: string, data: Client[] }, Error>({
+        queryKey: ["clients"],
+        queryFn: () => {
+            return fetch("http://localhost:3000/api/clients")
+                .then(res => res.json())
+        }
+    })
+
+    if (isLoading) {
+        return (
+            <div className="flex justify-center items-center h-full">
+                <span className="loading loading-ring loading-xl mt-20"></span>
+            </div>
+        )
+    }
+
+    if (isError) {
+        console.log(error)
+        return (
+            <>
+                <div className="flex flex-col items-center">
+                    <h3 className="text-center mt-10 text-xl text-error">Unable to fetch clients</h3>
+                    <button className="btn-warning btn rounded-full mt-5" onClick={() => refetch()}>Retry</button>
+                </ div>
+            </>
+        )
+    }
+
+    if (data) {
+        clients = data.data
+    }
 
     return (
         <div className="overflow-x-auto mt-10">
@@ -25,20 +53,32 @@ const TableList: React.FC = () => {
                     </tr>
                 </thead>
                 <tbody className="hover:bg-base-300">
-                    {clients.map(client => (
-                        <tr key={client.id}>
-                            <th>{client.id}</th>
-                            <td>{client.name}</td>
-                            <td>{client.email}</td>
-                            <td>{client.job}</td>
-                            <td>{client.rate}</td>
-                            <td><button className={`btn rounded-full w-20 ${client.isActive ? "btn-success" : "btn-outline btn-primary"}`}>{client.isActive ? "Active" : "Inactive"}</button></td>
-                            <td><button className="btn-warning btn rounded-full" onClick={() => {modalFormDispatch({type: "OPEN_MODAL", payload: {mode: "edit", data: client}})}}>Update</button></td>
-                            <td><button className="btn-error btn rounded-full">Delete</button></td>
-                        </tr>
-                    ))}
+                    {
+                        clients.length ? (
+                            clients.map(client => (
+                                <tr key={client.id}>
+                                    <th>{client.id}</th>
+                                    <td>{client.name}</td>
+                                    <td>{client.email}</td>
+                                    <td>{client.job}</td>
+                                    <td>{client.rate}</td>
+                                    <td><button className={`btn rounded-full w-20 ${client.isActive ? "btn-success" : "btn-outline btn-primary"}`}>{client.isActive ? "Active" : "Inactive"}</button></td>
+                                    <td><button className="btn-warning btn rounded-full" onClick={() => { modalFormDispatch({ type: "OPEN_MODAL", payload: { mode: "edit", data: client } }) }}>Update</button></td>
+                                    <td><button className="btn-error btn rounded-full">Delete</button></td>
+                                </tr>
+                            ))
+                        ) : <></>
+                    }
                 </tbody>
             </table>
+            {
+                !clients.length && (
+                    <div className="flex flex-col items-center mt-10">
+                        <h3 className="text-center">You have no Clients</h3>
+                        <button className="btn btn-info mt-5 rounded" onClick={() => { modalFormDispatch({ type: "OPEN_MODAL", payload: { mode: "add", data: { name: "", id: "", email: "", job: "", isActive: false, rate: ""} } }) }}>Add a Client</button>
+                    </div>
+                )
+            }
         </div>
     )
 }
